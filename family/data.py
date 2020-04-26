d6d9c59ba38a7b40e50ce746a9fea9a5f56534a8
Maria Iliukha <maria.iliukha@gmail.com>
2020-04-26 17:28:38 +0300

    removed

--- family/data.py
+++
@@ -1,7 +0,0 @@
-my_family = [
-                         {'name': 'Pups', 'birthday': '11/04/91', 'mobile': '0967710320', 'email': 'iliuha91@gmail.com'},
-                         {'name': 'Mom', 'birthday': '02/10/67', 'mobile': '0667242264', 'fb': 'Plotnikova Elena'},
-                         {'name': 'Dad', 'birthday': '18/02/66', 'mobile': '0667242263', 'fb': 'Plotnikov Oleg'},
-                         {'name': 'Sister', 'birthday': '28/02/86', 'mobile': '0675552264', 'fb': 'Kovryga Ekaterina', 'email': 'k.kovryha@gmail.com'},
-                         {'name': 'Me', 'birthday': '12/09/92', 'mobile': '0985800850', 'fb': 'Plotnikova Maria', 'email': 'm.plotniikova@gmail.com'}
-]
--- family/main_module.py
+++
@@ -1,5 +0,0 @@
-from family.classes import FindList
-from family.data import my_family as data
-
-f1 = FindList(data)
-print f1.get_closest_birthday()
